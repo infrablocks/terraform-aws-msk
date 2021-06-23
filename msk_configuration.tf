9719312a1c2da1293dda677c2b53507d2ad6f646
Jordan Robinson <me@jordanrobinson.co.uk>
2021-06-23 10:19:02 +0100

    Makes server properties configurable with defaults

--- msk_configuration.tf
+++ msk_configuration.tf
@@ -2,20 +2,5 @@ resource "aws_msk_configuration" "msk_config" {
   kafka_versions = [var.kafka_version]
   name           = "${var.component}-${var.deployment_identifier}-${var.configuration_name}"
 
-  server_properties = <<PROPERTIES
-auto.create.topics.enable=false
-default.replication.factor=3
-min.insync.replicas=2
-num.io.threads=8
-num.network.threads=5
-num.partitions=1
-num.replica.fetchers=2
-replica.lag.time.max.ms=30000
-socket.receive.buffer.bytes=102400
-socket.request.max.bytes=104857600
-socket.send.buffer.bytes=102400
-unclean.leader.election.enable=true
-zookeeper.session.timeout.ms=18000
-PROPERTIES
-
+  server_properties = var.server_properties
 }
